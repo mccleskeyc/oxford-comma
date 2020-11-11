edf5e46764798bec08e62ff1b103265dd1cbc738
mccleskeyc <mccleskeyc@gmail.com>
2020-11-11 15:20:58 -0500

    commit tree 56f140760c4a803501d80c1831779f3d0d1de573

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+    if array.length == 1
+        return "#{array [0]}"
+    elsif array.length == 2
+        return "#{array [0]} and #{array [1]}"
+    elsif array.length == 3
+        return "#{array [0]}, #{array [1]}, and #{array [2]}"
+    else
+        return array[0..-2].join(', ') + ", and " + array[-1]
+    end
+    
+  end
